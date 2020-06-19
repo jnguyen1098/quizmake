c1b69b84cc66282010c6fc9426fcff2d60be0dd2
jnguyen1098 <jnguyen1098@gmail.com>
2020-06-19 02:33:34 -0400

    Add metadata to `__init__.py`

--- quizmake/__init__.py
+++ quizmake/__init__.py
@@ -1,7 +1,15 @@
 """
 Initiation time
 """
+__all__ = ["Corpus", "corn"]
+__author__ = "Jason Nguyen"
+__copyright__ = "Copyright 2020, Jason Nguyen"
+__credits__ = "TBA"
+__license__ = "MIT"
+__maintainer__ = "Jason Nguyen"
+__email__ = "jnguye21@uoguelph.ca"
+__status__ = "Planning"
+with open("VERSION", "r") as fd:
+    __version__ = fd.read().strip()
 
 from .corpus import Corpus, corn
-
-__all__ = ["Corpus", "corn"]
